502ef5dd0e2efe98a007c1009f7904bc567d5eaa
Artem Shkuratov <shuratov2001@mail.ru>
2021-02-24 20:06:04 +0800

    Started project

---
+++ src/Footer.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './App.css';
+
+export default function Footer() {
+    return(
+        <footer className="footer">
+            <h2>footer (низ сайта)</h2>
+        </footer>
+    )
+}
---
+++ src/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './App.css';
+
+export default function Header(){
+    return(
+        <header className="header">
+            <img src='https://www.freelogodesign.org/Content/img/logo-ex-7.png' />
+        </header>
+    )
+}
