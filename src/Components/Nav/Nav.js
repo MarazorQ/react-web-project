0f95a26f947b52de88e61862ee2c74a4b0939b30
Artem Shkuratov <shuratov2001@mail.ru>
2021-02-25 17:27:07 +0800

    added NavLink that removed page reload

--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -1,23 +1,24 @@
 import React from 'react';
 import Styles from './Nav.module.css';
+import {NavLink} from 'react-router-dom';
 
 function Nav(){
     return(
         <nav className={Styles.nav}>
             <div className={Styles.item}>
-                <a href='/Profile'>Profile</a>
+                <NavLink to='/Profile' activeClassName={Styles.active}>Profile</NavLink>
             </div>
-            <div>
-                <a href='/Messages'>Messages</a>
+            <div className={Styles.item}>
+                <NavLink to='/Messages' activeClassName={Styles.active}>Messages</NavLink>
             </div>
-            <div>
-                <a>News</a>
+            <div className={Styles.item}>
+                <NavLink to='/News' activeClassName={Styles.active}>News</NavLink>
             </div>
-            <div>
-                <a>Music</a>
+            <div className={Styles.item}>
+                <NavLink to='/Music' activeClassName={Styles.active}>Music</NavLink>
             </div>
-            <div>
-                <a>Settings</a>
+            <div className={Styles.item}>
+                <NavLink to='/Settings' activeClassName={Styles.active}>Settings</NavLink>
             </div>
         </nav>
     )
