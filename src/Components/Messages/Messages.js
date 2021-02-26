48d911d5397ae6d8ec1e46f0c341688c3b1319c2
Artem Shkuratov <shuratov2001@mail.ru>
2021-02-26 16:19:12 +0300

    added styles for message component

--- src/Components/Messages/Messages.js
+++ src/Components/Messages/Messages.js
@@ -4,7 +4,27 @@ import Styles from './Messages.module.css';
 const Messages = () =>{
     return(
         <div className={Styles.content}>
-            Messages
+            <div className={Styles.dialogs}>
+                <div className={Styles.dialogsItems}>
+                    <div className={Styles.item}>
+                        Artem
+                    </div>
+                    <div className={Styles.item}>
+                        Pasha
+                    </div>
+                    <div className={Styles.item}>
+                        Sasha
+                    </div>
+                    <div className={Styles.item}>
+                        Roma
+                    </div>
+                </div>
+            </div>
+            <div className={Styles.messages}>
+                <div className={Styles.message}>sdasdasd</div>
+                <div className={Styles.message}>dasdasd</div>
+                <div className={Styles.message}>dasdaasd</div>
+            </div>
         </div>
     )
 }
