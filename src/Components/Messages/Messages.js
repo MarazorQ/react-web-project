067f333eb61bc531b45f8c69609185a469d4fd0f
Artem Shkuratov <shuratov2001@mail.ru>
2021-02-26 17:39:10 +0300

    changed structure of dialog component

--- src/Components/Messages/Messages.js
+++ src/Components/Messages/Messages.js
@@ -1,29 +1,41 @@
 import React from 'react';
 import Styles from './Messages.module.css';
+import {NavLink} from "react-router-dom";
+
+
+const DialogItem = (props) => {
+    let path = '/Messages/' + '' + props.id;
+    return(
+        <div className={Styles.item}>
+            <NavLink activeClassName={Styles.active} to={path}>{props.name}</NavLink>
+        </div>
+    )
+}
+
+const Message = (props) => {
+    return(
+        <div className={Styles.message}>{props.message}</div>
+    )
+}
 
 const Messages = () =>{
     return(
         <div className={Styles.content}>
             <div className={Styles.dialogs}>
                 <div className={Styles.dialogsItems}>
-                    <div className={Styles.item}>
-                        Artem
-                    </div>
-                    <div className={Styles.item}>
-                        Pasha
-                    </div>
-                    <div className={Styles.item}>
-                        Sasha
-                    </div>
-                    <div className={Styles.item}>
-                        Roma
-                    </div>
+                    <DialogItem name="Artem" id="2"/>
+                    <DialogItem name="Pasha" id="3"/>
+                    <DialogItem name="Roma" id="4"/>
+                    <DialogItem name="Artut" id="5"/>
+                    <DialogItem name="Dasha" id="7"/>
+                    <DialogItem name="Art" id="11"/>
+                    <DialogItem name="Rutu" id="24"/>
                 </div>
             </div>
             <div className={Styles.messages}>
-                <div className={Styles.message}>sdasdasd</div>
-                <div className={Styles.message}>dasdasd</div>
-                <div className={Styles.message}>dasdaasd</div>
+                <Message message="How are you"/>
+                <Message message="Fuck you"/>
+                <Message message="Hi!"/>
             </div>
         </div>
     )
