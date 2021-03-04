61cad2d13efad524f44fb8ffda2820e23fe6b0ab
Artem Shkuratov <shuratov2001@mail.ru>
2021-03-04 15:48:05 +0300

    added event and ref for input

--- src/Components/Content/Posts/Posts.js
+++ src/Components/Content/Posts/Posts.js
@@ -3,13 +3,18 @@ import Post from './Post/Post';
 
 const Posts = (props) =>{
     let NewPostItems = props.items.map(items => <Post name={items.name} count={items.countLike}/>)
+    let LinkRef = React.createRef()
+    let HandleButton = () =>{
+        let text = LinkRef.current.value;
+        alert(text)
+    }
     return(
         <div>
         My posts
         <div>
             <h2>New posts</h2>
-            <textarea></textarea>
-            <button>Add post</button>
+            <textarea ref={LinkRef}></textarea>
+            <button onClick={HandleButton} >Add post</button>
         </div>
             {NewPostItems}
         </div>
